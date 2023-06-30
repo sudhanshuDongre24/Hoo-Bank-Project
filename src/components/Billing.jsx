793448addef0e4582b8628ea96e34821fbb35f72
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-06-30 14:13:24 +0530

    inital commit

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
-const Billing = () => {
-  return <div>Billing</div>;
-};
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+
+      {/* Gradient */}
+      <div className="absolute z-[3] left-1/2 -top-[10%] w-[50%] h-[50%] rounded-full white__gradient"></div>
+      <div className="absolute z-[0] -left-[10%] bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"></div>
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" />
+        Billing & invoicing.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit neum sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neqie. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+
+      <div className="flex flex-row flew-wrap sm:mt-10 mt-6">
+        <img
+          src={apple}
+          alt="google_play"
+          className="w-[128px] h-[42px]
+        object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google}
+          alt="google_play"
+          className="w-[128px] h-[42px]
+        object-contain  cursor-pointer"
+        />
+      </div>
+    </div>
+  </section>
+);
 
 export default Billing;
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return <div>Clients</div>;
-};
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] hover:brightness-[900%]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[193px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients;
